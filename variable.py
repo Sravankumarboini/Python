a778bcbac6edd96c7410ea8480b191b736959d6e
Sravankumarboini <sravankumarboini005@gmail.com>
2025-11-16 19:48:43 +0530

    operators

--- variable.py
+++ variable.py
@@ -1,6 +1,34 @@
+#Basics of python
+'''
+Multi line comment
+'''
 name = "Sravan"
 age = 21
 PI = 3.14
 
 print("my name is :", name)
 print("my age is :", age)
+
+print(type(name),type(age),type(PI))
+
+full_name = "Boini Sravan kumar"
+
+print(full_name)
+
+a=5
+b=10
+
+sum=a+b 
+print("sum :",sum)
+
+#operators
+#arithmetic
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+#relational operator
