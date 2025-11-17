4eb8312c7fd74918a50fa0401890b57ac409122b
Sravankumarboini <sravankumarboini005@gmail.com>
2025-11-17 20:08:45 +0530

    match case

--- conditional.py
+++ conditional.py
@@ -1,6 +1,20 @@
+'''
 age=int(input("Enter your age : "))
 
 if(age>18) :
     print("You can vote")
 else :
     print("You can't vote")
+'''
+
+#match case(like switch)
+
+color = input("enter color: ")
+
+match color:
+    case "green":
+        print("Go")
+    case "yellow":
+        print("look")
+    case "red":
+        print("stop")
