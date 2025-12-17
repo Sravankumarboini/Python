6e18c3fbaebc9b4a8f8d170ac3f918d33f888551
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-17 18:46:54 +0530

    set

---
+++ set.py
@@ -0,0 +1,24 @@
+#sets (collection of unique elements)
+empty_set = set()
+
+set = {1,2,3,4,4,5}
+
+print(set)
+
+#methods
+
+set.add(6)
+
+set.remove(1)
+
+print(set)
+
+set.pop()
+
+print(set)
+
+set2 = {11,12,5,4}
+
+print(set.union(set2))
+
+print(set.intersection(set2))
