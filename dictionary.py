f77dfff1c6b362f3cf5dffde711636368559588f
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-17 18:33:30 +0530

    dictionary methods

--- dictionary.py
+++ dictionary.py
@@ -1,10 +1,24 @@
 #dictionary(key:value pairs)  Unique
 
 dict = {
-    "name:"sravan kumar",
+    "name":"sravan kumar",
     "subject" : "AI & ML",
     "status" : "beginner"
 }
 print(dict)
 print(type(dict))
 print(dict["name"])
+
+#methods
+
+print(dict.keys())
+
+print(dict.values())
+
+print(dict.items())
+
+print(dict.get("status"))
+
+dict.update({
+    "city" : "hyderabad"
+    })
