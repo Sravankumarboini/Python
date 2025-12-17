3afd699b99cd063eb16a1cf2a8e00c2c3000fa40
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-17 07:25:40 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,20 @@
+#tuple(immutable sequence of values)
+
+tuple = (1,2,3,4,5)
+
+print(tuple)
+print(len(tuple))
+
+print(tuple[3:])
+sum=0
+for val in tuple:
+    sum+=val
+print(sum)
+
+#index method(return first occurence idx)
+
+print(tuple.index(2))
+
+#count the value occurences
+
+print(tuple.count(10))
