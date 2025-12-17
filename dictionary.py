7b28317f60ed869d345e364cf70c427a08d794f3
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-17 18:26:05 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+#dictionary(key:value pairs)  Unique
+
+dict = {
+    "name:"sravan kumar",
+    "subject" : "AI & ML",
+    "status" : "beginner"
+}
+print(dict)
+print(type(dict))
+print(dict["name"])
