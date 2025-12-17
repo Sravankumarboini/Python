b5f398d3d5eb1db56408a762d1de12cdaa0c18fa
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-17 06:44:53 +0530

    f-string

--- string.py
+++ string.py
@@ -1,3 +1,4 @@
+#string is immmutable
 name = "sravan"
 
 print(len(name))
@@ -5,3 +6,20 @@ print(len(name))
 #conactinate
 
 print(name + "kumar")
+
+#slicing
+#str[st idx : end idx] ending index is not inclluded
+
+print(name[2:4])
+print(name[-4:-2])
+
+#string formatting(format and f-string)
+
+#format 
+#normal formatting
+print("sum of {} & {} is {}".format(10,15,25))
+#index based formatting
+print("sum of {1} & {0} is {2}".format(10,15,25))
+#value based formatting
+print("values of a= {a} & b={b}".format(a=5,b=10))
+
