d8ff004ea004cd94aac72a70c5c1151e79244965
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-17 07:06:20 +0530

    list methods

--- list.py
+++ list.py
@@ -7,3 +7,27 @@ marks[2] = 70
 
 print(marks)
 print(type(marks))
+
+#slicing
+
+print(marks[4:6])
+
+print(marks[-5:-2])
+
+#list methods
+nums = [1,2,3]
+#append
+nums.append(4)
+#insert element with index
+nums.insert(2,10)
+print(nums)
+
+#sort
+nums.sort()
+print(nums)
+nums.sort(reverse=True)
+print(nums)
+
+#reverse
+nums.reverse()
+print(nums)
