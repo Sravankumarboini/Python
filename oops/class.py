4f299b33d5b38899b6af48b9c3181caf7936329d
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-18 06:50:22 +0530

    class with multiple parameters

--- oops/class.py
+++ oops/class.py
@@ -1,12 +1,13 @@
 class student :
-    def __init__(self,name) :
+    def __init__(self,name,cgpa) :
         self.name = name
+        self.cgpa = cgpa
 
 
-stu1 = student("sravan kumar")
-stu2 = student("sandeep")
-stu3 = student("sathish")
+stu1 = student("sravan kumar",8.5)
+stu2 = student("sandeep",7.5)
+stu3 = student("sathish",7.5)
 
-print(stu1.name)
-print(stu2.name)
-print(stu3.name)
+print(stu1.name,stu1.cgpa)
+print(stu2.name,stu2.cgpa)
+print(stu3.name,stu3.cgpa)
