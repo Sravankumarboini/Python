a482519655ec9e2c058f6143fa1ebbd62ace17fd
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-18 14:01:55 +0530

    product problem using class and methods

---
+++ oops/product.py
@@ -0,0 +1,27 @@
+class product:
+    count = 0
+
+    def __init__(self,name,price):
+        self.name = name
+        self.price = price
+        product.count+=1
+
+    def get_info(self):#instance method
+        print(f"price of {self.name} is {self.price}")
+
+    @classmethod
+    def get_count(cls):
+        print(f"total products in store = {cls.count}")
+
+    @staticmethod
+    def calc_discount(price, discount):
+        print(f"discount price is {price - (price*discount/100)}")
+
+p1 = product("phone", 10_000)
+p2 = product("laptop", 50_000)
+p3 = product("pen", 10)
+
+p1.get_info()
+product.get_count()
+
+p1.calc_discount(10_000,12)
