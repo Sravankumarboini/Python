4d9d205932ef5e25071e6173ba26de36863546ea
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-18 06:55:31 +0530

    get method

--- oops/class.py
+++ oops/class.py
@@ -1,7 +1,9 @@
 class student :
-    def __init__(self,name,cgpa) :
+    def __init__(self,name,cgpa) :#self is reference to that particular object
         self.name = name
         self.cgpa = cgpa
+    def get_cgpa(self) :
+        return self.cgpa
 
 
 stu1 = student("sravan kumar",8.5)
@@ -11,3 +13,5 @@ stu3 = student("sathish",7.5)
 print(stu1.name,stu1.cgpa)
 print(stu2.name,stu2.cgpa)
 print(stu3.name,stu3.cgpa)
+
+print(stu1.get_cgpa())
