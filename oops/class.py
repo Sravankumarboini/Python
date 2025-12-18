9c9325e76d34d0016876605f9986eb491e0c32e4
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-18 13:37:35 +0530

    class and instance method

--- oops/class.py
+++ oops/class.py
@@ -19,7 +19,7 @@ print(stu1.get_cgpa())
 
 class stu :
     college_name = "abc college" #class attribute
-    
+
     def __init__(self, name, cgpa) :
         self.name = name #object atrribute(these changes according to object)
         self.cgpa = cgpa
@@ -30,3 +30,24 @@ print(st1.college_name)
 print(stu.college_name)
 print(st1.name)
 print(st1.cgpa)
+
+
+class Laptop :
+    storage_type = "ssd"
+
+    def __init__(self,RAM,storage):
+        self.RAM = RAM
+        self.storage = storage
+    
+    @classmethod #decorator
+    def get_storage_type(cls):#class method
+        print(f"storage_type = {cls.storage_type}")
+
+    def get_info(self): #instance method
+        print(f"laptop has {self.RAM} & {self.storage} {self.storage_type}")
+l1 = Laptop("16gb", "512gb")
+l2 = Laptop("8gb", "256gb")
+
+l1.get_info()
+
+l1.get_storage_type()
