f6f8a0b8e4d2d9ec16c62dd119a35192adcb977a
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-18 06:48:03 +0530

    class with parameters

--- oops/class.py
+++ oops/class.py
@@ -1,6 +1,12 @@
 class student :
-    def __init__(self) :
-        print("something")
+    def __init__(self,name) :
+        self.name = name
 
 
-stu1 = student()
+stu1 = student("sravan kumar")
+stu2 = student("sandeep")
+stu3 = student("sathish")
+
+print(stu1.name)
+print(stu2.name)
+print(stu3.name)
