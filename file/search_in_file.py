30e812b610247f69de55328d5983d30dd78d4aab
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-22 17:56:06 +0530

    word search in file

---
+++ file/search_in_file.py
@@ -0,0 +1,12 @@
+data = True
+line = 1
+word = input("enter the word : ")
+with open("simple.txt", "r") as f:
+
+    while data:
+        data = f.readline()
+
+        if(word in data):
+            print(f"{word} found at line {line}")
+            break
+        line+=1  
