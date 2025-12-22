d984883325b3287702a9efdead2ca74957080898
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-22 20:48:23 +0530

    exception handling

---
+++ ExceptionHandling/code.py
@@ -0,0 +1,12 @@
+try:
+    x = int(input("enter x value :"))
+    ans = 10/x
+
+except ZeroDivisionError:
+    print(f"Divide by zero is not allowed")
+    
+except ValueError:
+    print("Invalid input")
+
+else:
+    print(f"ans = {ans}")
