c60f01d893e7d621998cd286b451f3e20042bc84
Sravankumarboini <sravankumarboini005@gmail.com>
2025-12-25 10:49:32 +0530

    jsonmodule

--- JSON_Module/code.py
+++ JSON_Module/code.py
@@ -9,3 +9,10 @@ print(type(py_obj), py_obj)
 json_str1 = json.dumps(py_obj)#converts python object to json string
 
 print(type(json_str1), json_str1)
+
+#------------------------------------------------------------------------
+#to deal with files
+
+with open("data.json", "r") as f:
+    py_obj1 = json.load(f)
+    print(type(py_obj1), py_obj1)
